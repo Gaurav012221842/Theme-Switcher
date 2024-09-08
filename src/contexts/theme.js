98ddab8e47c446f852e02350f903a9abfae24f49
Gaurav Kumar <gauravkumar@Gauravs-MacBook.local>
2024-09-08 23:46:00 +0530

    Switching Theme

---
+++ src/contexts/theme.js
@@ -0,0 +1,11 @@
+import { createContext,useContext } from "react";
+import React from "react";
+export const ThemeContext=createContext({
+    thememode:"light",
+    darkTheme: ()=>{},
+    lightTheme: ()=>{}
+})
+export const ThemeProvider=ThemeContext.Provider
+export default function useTheme(){
+    return useContext(ThemeContext)
+}
